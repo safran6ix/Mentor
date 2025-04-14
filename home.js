c5b41efa66c3607ab0e336c44e7ef3df102787a1
safran6ix <mohammedsafran6ix@gmail.com>
2025-04-14 23:33:35 +0530

    Modified

---
+++ home.js
@@ -0,0 +1,8 @@
+const sidenavbar = document.querySelector(".side-navbar");
+
+function showBar(){
+    sidenavbar.style.left = "0";
+}
+function noneShowBar(){
+    sidenavbar.style.left = "-100%";
+}
